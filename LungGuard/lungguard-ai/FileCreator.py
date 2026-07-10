1583e3a74aa65f73a1f6637fdd75f8f34f9c5170
akashreddy07 <149877503+akashreddy07@users.noreply.github.com>
2026-07-10 15:49:32 +0530

    Update FileCreator.py

--- LungGuard/lungguard-ai/FileCreator.py
+++ LungGuard/lungguard-ai/FileCreator.py
@@ -1,8 +1,1 @@
-from random import choice
-f = open("Temp.txt","a+")
-msg = []
-for _ in range(100000):
-    ch = choice(['Ateeq','Rahil','Akash','Moki'])
-    msg.append(f"Hello {ch}, bye\n")
-f.writelines(msg)
-f.close()
+
